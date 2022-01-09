00f0de576d0905b6040312fc38b4df8809f4d78a
Akira  Shigeta <shige@cdrive.jp>
2022-01-09 21:57:33 +0900

    プログラム制作中

---
+++ getallpagedownloads.js
@@ -0,0 +1,51 @@
+var client = require('cheerio-httpcli');
+var request = require('request');
+var URL = require('url');
+var fs = require('fs');
+var path = require('path');
+// 階層の指定
+var LINK_LEVEL = 3;
+// 基準となるページURL
+var TARGET_URL = "https://nodejs.org/dist/latest-v17.x/docs/api/";
+var list = {};
+// ダウンロード関数実行
+download2rec(TARGET_URL, 0);
+
+
+function download2rec(url, level){
+    // 最大レベルチェック
+    if(level >= LINK_LEVEL) return;
+    // 重複ダウンロードチェック
+    if(list[url]) return;
+    list[url] = true;
+    // 基準ページ以外なら無視する
+    var us = TARGET_URL.split("/");
+    us.pop();
+    var base = us.join("/");
+    if (url.indexOf(base) < 0) return;
+    // HTMLを取得する
+    client.fetch(url, {}, function (err, $, res){
+        // リンクされているページを取得
+        $("a").each(function(idx){
+            // <a> リンク先を取得する
+            var href = $(this).attr("href");
+            if (!href) return;
+            // 絶対パスを相対パスに変換
+            href = URL.resolve(url, href);
+            // # 以降のURIを無視する
+            href = href.replace(/\#.+$/,"");
+            //console.log(href);
+            //console.log(href.indexOf(base));
+            download2rec(href, level + 1);
+        });
+        // ページを保存ファイル名を決める
+        if(url.substr(url.length-1, 1) == "/"){
+            url += "index.html";
+        }
+        var savepath = url.split("/").slice(2).join("/");
+        console.log(savepath);
+    });
+}
+
+
+function checkSave2Dir() {}
--- readme.md
+++ readme.md
@@ -1,4 +1,15 @@
 # サイト内リンクページダウンロード
 
 トップページからリンクされているページを巡回して１ページ毎ダウンロードしていくプログラムになります。
+まずは３階層までリンクしているHTMLファイルをしらべて、ローカルにHTMLファイルをダウンロードしていきます。
 
+```bash
+npm i cheerio-httpcli request
+```
+
+getallpagedownloads.js
+
+```javascript
+var client = require('cheerio-httpcli');
+
+```
